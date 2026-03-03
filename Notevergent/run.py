6b628235ab70756e43d8ed0d09fa1d636a2946de
JaguarKaptan <efecantaha34@gmail.com>
2026-03-03 23:22:01 +0300

    Vercel Update

--- Notevergent/run.py
+++ Notevergent/run.py
@@ -6,4 +6,4 @@ flask_app = create_app()
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    flask_app.run(host='0.0.0.0', port=port ,debug=True)
+    flask_app.run(host='0.0.0.0', port=port ,debug=False)
