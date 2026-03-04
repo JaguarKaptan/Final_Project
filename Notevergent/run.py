81403765d10f8c2fa8bf89230510d8af7c06d345
JaguarKaptan <efecantaha34@gmail.com>
2026-03-04 16:27:09 +0300

    Deployment Issue Fixes

--- Notevergent/run.py
+++ Notevergent/run.py
@@ -1,9 +1,17 @@
-import os
+# import os
 
+# from app import create_app
+
+# flask_app = create_app()
+
+# if __name__ == '__main__':
+#     port = int(os.environ.get("PORT", 5000))
+#     flask_app.run(host='0.0.0.0', port=port ,debug=False)
+
+import os
 from app import create_app
 
-flask_app = create_app()
-
+app = create_app() 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    flask_app.run(host='0.0.0.0', port=port ,debug=False)
+    app.run(host='0.0.0.0', port=port, debug=False)
